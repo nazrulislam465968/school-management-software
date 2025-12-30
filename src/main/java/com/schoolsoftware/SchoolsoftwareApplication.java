2325e84a2ae0216d55050e32c4d9a8c358b3398e
nazrul-islam <nazruldeeds@gmail.com>
2025-12-30 23:43:41 +0600

    Initial commit: clean base structure

---
+++ src/main/java/com/schoolsoftware/SchoolsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.schoolsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolsoftwareApplication.class, args);
+	}
+
+}
